b0c768fd25ac4d4b8d478bddc96019a3d7b538cb
Ruben Arriazu <ruben.arriazu.simon@gmail.com>
2021-03-10 08:41:36 +0100

    Introduce contenedores

---
+++ create-container.sh
@@ -0,0 +1,8 @@
+# Crea un contenedor llamado comercio-online
+
+docker run --name comercio-online \
+-v $PWD:/db-contenedor:z \
+-p 3307:3306 \
+-e MYSQL_ALLOW_EMPTY_PASSWORD=secret \
+mysql:8.0 \
+--secure-file-priv=/db-contenedor/Datos
