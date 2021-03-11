fa1efeeac6216f9dc017f291ede131a12477dd5a
Ruben Arriazu <ruben.arriazu.simon@gmail.com>
2021-03-11 10:22:30 +0100

    cambios V

--- consultas.sql
+++ consultas.sql
@@ -10,27 +10,26 @@ group by vendedor.id_vendedor order by TOTAL desc;
 -- Mostrar cuántos vendedores existen en cada categoria  ordenada por el número de vendedores descendentemente 
 -- select categoria.nombre as categoria, count(vendedor.id_vendedor) as total_vendedores
 select categoria.nombre as categoria, count(distinct vendedor.id_vendedor) as total_vendedores
-from vendedor, categoria, producto
+from vendedor, categoria, producto 
 where producto.id_vendedor = vendedor.id_vendedor and producto.id_categoria=categoria.id_categoria
 group by categoria.id_categoria
 order by total_vendedores desc;
 
 -- Mostrar el nombre de los prodcutos de la categoria Frutas
-select producto.nombre
+select distinct producto.nombre as frutas_en_stock
 from producto, categoria
 where producto.id_categoria=categoria.id_categoria and categoria.nombre='Frutas';
 
 -- Mostrar el producto que mas veces se ha comprado y cuantas veces
-select producto.nombre, count(pedido.id_producto) as total from producto, pedido
-where producto.id_producto=pedido.id_producto group by pedido.id_producto order by total desc;
+select producto.nombre, count(pedido.id_producto) as veces_vendido from producto, pedido
+where producto.id_producto=pedido.id_producto group by pedido.id_producto order by veces_vendido desc limit 1;
 
 -- Mostrar el precio del producto con el precio mas caro y el mas barato
-select producto.precio from producto
-where producto.precio = (select max(producto.precio) from producto) 
-or producto.precio = (select min(producto.precio) from producto);
+select precio from producto
+where precio = (select max(precio) from producto) 
+or precio = (select min(precio) from producto);
 
--- Mostrar el nombre y apellidos de los 10 clientes que más compras han realizado
--- Mostrar tambien el total de las compras
+-- Mostrar el nombre y apellidos de los 10 clientes que más cantidad de productos han comprado y cuántos
 select cliente.nombre, cliente.apellidos, sum(pedido.cantidad) as total 
 from cliente , pedido
 where cliente.dni = pedido.dni_cliente
@@ -38,11 +37,12 @@ group by pedido.dni_cliente order by total desc
 limit 10;
 
 -- Mostrar telefono de los clientes que compraron en el mes de Abril
-select distinct cliente.nombre, cliente.tlfn, pedido.fecha from cliente, pedido where cliente.dni = pedido.dni_cliente
-and month(pedido.fecha)=04;
+select distinct cliente.nombre, cliente.tlfn, pedido.fecha
+from cliente, pedido
+where cliente.dni = pedido.dni_cliente and month(pedido.fecha)=04;
 
 -- Mostrar los distintas categorias que hay en la tienda online
-select categoria.nombre from categoria group by categoria.nombre;
+select nombre as categorias from categoria group by nombre;
 
 -- Mostrar la media de ventas por mes en el primer semestre del año 2020 
 select truncate(avg(total),2) as total_ventas, month(fecha)
